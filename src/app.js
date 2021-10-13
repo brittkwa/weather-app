bcfbb9dbdecc51bace54c487e0cb8a3a83f0bcf0
Brittney K Walker <brittkwa@gmail.com>
2021-10-13 15:19:31 -0400

    edited changeCurrentWeather to get the icon change working, added some comments, removed unnecessary code

--- src/app.js
+++ src/app.js
@@ -34,6 +34,35 @@ let apiURLCity = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid
 let apiURLOneCall= `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 let apiURLCoords =`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 let apiIconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+let form = document.querySelector("form");
+form.addEventListener("submit", searchToCoords);
+
+
+
+
+
+//accepting (if applicable) the geolocation information
+  let locationButton = document.querySelector("#cLocationBtn").addEventListener("click", function getCurrentLocation(event){
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(coordsLocationVar);
+  
+})
+
+//takes the search input and uses that to get the API results which have the coords
+function searchToCoords(event){
+  event.preventDefault();
+  let inputCity = document.querySelector("#city-input").value;
+  apiURLCity = `http://api.openweathermap.org/data/2.5/weather?q=${inputCity}&appid=${apiKey}`;
+  axios.get(apiURLCity).then(logLocation);
+}
+
+//logs the api response to console and sets longitude and latitude variables, runs getWeather
+function logLocation(response){
+  console.log(response);
+  lat = response.data.coord.lat;
+  lon = response.data.coord.lon;
+  getWeather();
+}
 
 //gets the current date and dates for the 7 day forecast
 function getDates() {
@@ -71,30 +100,22 @@ function getDates() {
   }
 }
 
-
-//accepting (if applicable) the geolocation information
-  let locationButton = document.querySelector("#cLocationBtn").addEventListener("click", function getCurrentLocation(event){
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(coordsLocationVar);
-  
-})
-
 //changes the position object to latitude and longitude 
 function coordsLocationVar(position){
   lat = position.coords.latitude;
   lon = position.coords.longitude;
   apiURLCoords =`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
-  console.log(lat);
-  console.log(lon);
   getWeather();
 }
 
 //retrieves the current weather for the selected coords through an API call
 function getWeather(){
-  axios.get(apiURLCoords).then(logResponse);
+  apiURLCoords =`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
+  axios.get(apiURLCoords).then(logWeatherResponse);
 }
 
-function logResponse(response){
+//logs the API response and launches processCurrentWeather function
+function logWeatherResponse(response){
   console.log(response.data);
   processCurrentWeather(response.data);
 }
@@ -103,20 +124,21 @@ function processCurrentWeather(response){
   tempHigh = response.main.temp_max;
   tempLow = response.main.temp_min;
   tempNow = response.main.temp;
-  icon = response.weather.icon;
+  icon = response.weather[0].icon;
+  console.log(icon);
   apiIconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  
   changeCurrentWeather();
 }
 
+//changes the innerHTML to the proper current (today) weather
 function changeCurrentWeather(){
   let cTempHigh = document.getElementById("currentHigh");
   let cTempLow = document.getElementById("currentLow");
   let cTempNow = document.getElementById("currentTemp");
-  let icon1 = document.querySelector(".icon1");
   cTempLow.innerHTML = tempLow;
   cTempHigh.innerHTML = tempHigh;
   cTempNow.innerHTML = tempNow;
-  icon1.innerHTML = apiIconURL;
-  
+  document.getElementById("icon1").src = apiIconURL;
 }
 
